591362b119c1839522699b456b2b9688ccaca657
Almudena Hellin <97666628+AlmudenaHell@users.noreply.github.com>
2026-04-22 14:12:40 +0200

    Delete src directory

--- src/ingest.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def load_articles(data_path="data"):
-    articles = []
-    for file in os.listdir(data_path):
-        if file.endswith(".txt"):
-            with open(os.path.join(data_path, file), "r") as f:
-                articles.append(f.read())
-    return articles
